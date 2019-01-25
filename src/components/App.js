a568a37bc39590e88a6df156185ed03d80517786
Dony Cisneros <dony.cisne@gmail.com>
2019-01-25 19:15:59 -0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Player from './Player';
+
+class App extends Component {
+  state = {
+    players: [
+      {
+        name: 'Dony',
+        score: 0,
+        id: 1,
+      },
+      {
+        name: 'Rose',
+        score: 0,
+        id: 2,
+      },
+      {
+        name: 'Sam',
+        score: 0,
+        id: 3,
+      },
+      {
+        name: 'Billy',
+        score: 0,
+        id: 4,
+      },
+    ],
+  };
+
+  handleScoreChange = (index, delta) => {
+    this.setState( prevState => ({
+      score: prevState.players[index].score += delta,
+    }));
+  };
+
+  handleRemovePlayer = id => {
+    this.setState( prevState => {
+      return {
+        players: prevState.players.filter(p => p.id !== id),
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div className="scoreboard">
+        <Header
+          title="Scoreboard"
+          totalPlayers={this.state.players.length}
+        />
+
+        {/* Players list */}
+        {this.state.players.map((player, index) =>
+          <Player
+            name={player.name}
+            score={player.score}
+            id={player.id}
+            key={player.id.toString()}
+            index={index}
+            changeScore={this.handleScoreChange}
+            removePlayer={this.handleRemovePlayer}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
